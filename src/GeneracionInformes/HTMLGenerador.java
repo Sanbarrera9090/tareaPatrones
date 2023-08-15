2be220a5ca51aa99e72dce0a95e1635aa6489edd
Santiago Barrera Salamanca <santiagobasa@unisabana.edu.co>
2023-08-14 19:55:52 -0500

    Logica/Interface para generar reportes

---
+++ src/GeneracionInformes/HTMLGenerador.java
@@ -0,0 +1,10 @@
+package GeneracionInformes;
+
+import GeneracionInformes.Interface.GeneradorReportes;
+
+public class HTMLGenerador implements GeneradorReportes {
+    @Override
+    public void generateReport() {
+
+    }
+}
